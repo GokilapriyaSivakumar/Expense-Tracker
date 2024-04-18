2b38838f315ddba852727c62216ed6c32b5a8af7
GokilapriyaSivakumar <gokilapriyas.21it@kongu.edu>
2024-04-18 22:19:19 +0530

    Committed

---
+++ expense-vite/src/Components/Balance.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Balance({ transactions }) {
+    const amounts = transactions.map(transaction =>transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item) , 0)
+    
+    
+  return (
+    
+    <>
+    <h3>Balance</h3>
+    <h1>${total}</h1></>
+
+  )
+}
+
+export default Balance
